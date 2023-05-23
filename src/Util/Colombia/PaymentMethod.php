7e8b7a4612ea73b7a061545b1b0d3e8e90e5ba66
Nat-liquido <nataliasantinello@liquido.com>
2023-05-23 17:35:00 -0300

    Moving Cash payment method from Colombia to Common class.

--- src/Util/Colombia/PaymentMethod.php
+++ src/Util/Colombia/PaymentMethod.php
@@ -5,6 +5,6 @@ namespace LiquidoBrl\PayInPhpSdk\Util\Colombia;
 abstract class PaymentMethod
 {
     const PSE = "PSE";
-    const CASH = "PAY_CASH";
+    //const CASH = "PAY_CASH";
     //const CREDIT_CARD = "CREDIT_CARD";
 }
--- src/Util/Common/PaymentMethod.php
+++ src/Util/Common/PaymentMethod.php
@@ -5,4 +5,5 @@ namespace LiquidoBrl\PayInPhpSdk\Util\Common;
 abstract class PaymentMethod
 {
     const CREDIT_CARD = "CREDIT_CARD";
+    const CASH = "PAY_CASH"; // Mexico and Colombia
 }
